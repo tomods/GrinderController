e1e60354629c7b33b1ee0776dde703585c0ad537
Tobias Modschiedler <mail@m-to.de>
2022-01-04 20:37:52 +0100

    Avoid floating point arithmetic AND loss of accuracy

--- grinder_filter.py
+++ grinder_filter.py
@@ -6,14 +6,13 @@ class GrinderFilter:
         self._filter_deque = deque(tuple(), filter_size)
         for _ in range(filter_size):
             self._filter_deque.append(initial_value)
-        self._value_filtered = initial_value
+        self._value_filtered = initial_value << 16
         self._filter_size = filter_size
 
     def filter_value(self, new_val: int) -> int:
         oldest = self._filter_deque.popleft()
         self._filter_deque.append(new_val)
-        # TODO: Avoid floating point math.
-        #       But DO NOT just use integer division - will introduce too much error!
-        self._value_filtered += (new_val - oldest) / self._filter_size
+        # Shift around a bit to avoid losing accuracy without using floating point arithmetic
+        self._value_filtered += ((new_val - oldest) << 16) // self._filter_size
 
-        return int(self._value_filtered)
+        return int(self._value_filtered >> 16)
