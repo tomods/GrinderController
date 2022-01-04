0c8e90cdbf48123a8038d9ea6774c39857e9abcb
Tobias Modschiedler <mail@m-to.de>
2022-01-04 20:22:30 +0100

    For the time being, use float in filter.

--- grinder_filter.py
+++ grinder_filter.py
@@ -2,17 +2,18 @@ from collections import deque
 
 
 class GrinderFilter:
-    def __init__(self, initial_value, filter_size):
+    def __init__(self, initial_value: int, filter_size):
         self._filter_deque = deque(tuple(), filter_size)
         for _ in range(filter_size):
             self._filter_deque.append(initial_value)
         self._value_filtered = initial_value
         self._filter_size = filter_size
 
-    def filter_value(self, new_val):
+    def filter_value(self, new_val: int) -> int:
         oldest = self._filter_deque.popleft()
         self._filter_deque.append(new_val)
-        # FIXME Is it actually a good idea to not use float here?
-        self._value_filtered += (new_val - oldest) // self._filter_size
+        # TODO: Avoid floating point math.
+        #       But DO NOT just use integer division - will introduce too much error!
+        self._value_filtered += (new_val - oldest) / self._filter_size
 
-        return self._value_filtered
+        return int(self._value_filtered)
